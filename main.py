ae03e16078dee680511e83f5931dfe81223faf94
Demo User <demouser@MacBook-Air.local>
2018-07-23 13:41:59 -0700

    Fixed commas

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class SubmitHandler(webapp2.RequestHandler):
     def get(self):
         main_template = jinja_env.get_template('templates/submit.html')
         html = main_template.render({
-            'title' : self.request.get('title')
-            'desc' : self.request.get('desc')
+            'title' : self.request.get('title'),
+            'desc' : self.request.get('desc'),
             'location' : self.request.get('location')
         })
         self.response.write(html)
@@ -26,8 +26,8 @@ class ViewHandler(webapp2.RequestHandler):
     def get(self):
         main_template = jinja_env.get_template('templates/view.html')
         html = main_template.render({
-            'title' : self.request.get('title')
-            'desc' : self.request.get('desc')
+            'title' : self.request.get('title'),
+            'desc' : self.request.get('desc'),
             'location' : self.request.get('location')
         })
         self.response.write(html)

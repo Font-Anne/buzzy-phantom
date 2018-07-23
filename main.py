2a756f1c00ee01d3db4449b5aa18546d2de82ccd
Jerry Jean-Pierre <jeanpierrejerry@gmail.com>
2018-07-23 12:43:24 -0400

    added classes

---
+++ main.py
@@ -0,0 +1,31 @@
+import webapp2
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        main_template = jinja_env.get_template('templates/main.html')
+        html = main_template.render()
+        self.response.write(html)
+
+class SubmitHandler(webapp2.RequestHandler):
+    def get(self):
+        main_template = jinja_env.get_template('templates/submit.html')
+        html = main_template.render()
+        self.response.write(html)
+
+class ViewHandler(webapp2.RequestHandler):
+    def get(self):
+        main_template = jinja_env.get_template('templates/view.html')
+        html = main_template.render()
+        self.response.write(html)
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/submit', SubmitHandler),
+    ('/view', ViewHandler),
+], debug=True)

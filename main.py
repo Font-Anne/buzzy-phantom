320e0eeb1b55fb7cc02fbb8b03fcbd3fdbbb4e70
Demo User <demouser@MacBook-Air.local>
2018-07-24 14:12:07 -0700

    IMAGES WORKING

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 import information
 from google.appengine.api import images
+from google.appengine.ext import ndb
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -21,6 +22,8 @@ class MainHandler(webapp2.RequestHandler):
             self.response.write("</div> <h2>" + post.title + "</h2>")
             self.response.write("<p></p><h3>" + post.desc + "</h3>")
             self.response.write("<p></p><p></p><h3>" + post.location + "</h3>")
+            if post.image:
+                self.response.write("<p></p> <img src='/img?id=" + str(post.key.id()) + "'>")
             self.response.write("</div>")
             self.response.write("<br></br>")
 
@@ -29,30 +32,28 @@ class MainHandler(webapp2.RequestHandler):
         data.title = self.request.get('title')
         data.desc = self.request.get('desc')
         data.location = self.request.get('location')
-        data.image = images.resize(self.request.get('image'), 32, 32)
+        data.image = images.resize(self.request.get('image'), 600, 600)
         data.put()
 
         main_template = jinja_env.get_template('templates/main.html')
         all_posts = information.Data.query().fetch()
 
-        html = main_template.render()
+        html = main_template.render({
+            "title": data.title,
+            "desc": data.desc,
+            "location": data.location,
+            "image": data.image
+        })
         self.response.write(html)
-
-        self.response.write("<div class= 'box'>")
-        self.response.write("<div id= 'post_image'>")
-        self.response.write("</div> <h2>" + data.title + "</h2>")
-        self.response.write("<p></p><h3>" + data.desc + "</h3>")
-        self.response.write("<p></p><p></p><h3>" + data.location + "</h3>")
-        self.response.write("</div>")
-        self.response.write("<br></br>")
-
+        self.response.write("<p></p>")
         for post in all_posts:
             self.response.write("<div class= 'box'>")
             self.response.write("<div id= 'post_image'>")
             self.response.write("</div> <h2>" + post.title + "</h2>")
             self.response.write("<p></p><h3>" + post.desc + "</h3>")
             self.response.write("<p></p><p></p><h3>" + post.location + "</h3>")
-            self.response.write("</div>")
+            if post.image:
+                self.response.write("<p></p> <img src='/img?id=" + str(post.key.id()) + "'>")
             self.response.write("<br></br>")
 
 class SubmitHandler(webapp2.RequestHandler):
@@ -61,7 +62,16 @@ class SubmitHandler(webapp2.RequestHandler):
         html = main_template.render()
         self.response.write(html)
 
+class Image(webapp2.RequestHandler):
+    def get(self):
+        key = ndb.Key("Data", int(self.request.get("id")))
+        data = key.get()
+        self.response.headers['Content-Type'] = 'image/jpg'
+        self.response.write(data.image)
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/submit', SubmitHandler),
+    ('/img', Image)
 ], debug=True)

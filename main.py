1774fc46f14d7d9beb423a3232af59a2393953eb
Demo User <demouser@MacBook-Air.local>
2018-07-24 13:12:58 -0700

    More image work

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ class MainHandler(webapp2.RequestHandler):
         html = main_template.render()
         self.response.write(html)
 
-    all_posts = information.Data.query().fetch()
-    for post in all_posts:
-        self.response.write("<div class= 'box'>")
-        self.response.write("<div id= 'post_image'>")
-        self.response.write("</div> <h2>" + post.title + "</h2>")
-        self.response.write("<p></p><h3>" + post.desc + "</h3>")
-        self.response.write("<p></p><p></p><h3>" + post.location + "</h3>")
-        self.response.write("</div>")
-        self.response.write("<br></br>")
+        all_posts = information.Data.query().fetch()
+        for post in all_posts:
+            self.response.write("<div class= 'box'>")
+            self.response.write("<div id= 'post_image'>")
+            self.response.write("</div> <h2>" + post.title + "</h2>")
+            self.response.write("<p></p><h3>" + post.desc + "</h3>")
+            self.response.write("<p></p><p></p><h3>" + post.location + "</h3>")
+            self.response.write("</div>")
+            self.response.write("<br></br>")
 
     def post(self):
         data = information.Data()
         data.title = self.request.get('title')
         data.desc = self.request.get('desc')
         data.location = self.request.get('location')
-        # data.image = images.resize(self.request.get('image'), 32, 32)
+        data.image = images.resize(self.request.get('image'), 32, 32)
         data.put()
 
         main_template = jinja_env.get_template('templates/main.html')

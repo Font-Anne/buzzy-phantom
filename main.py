22eca590f4de3260a00bbe778993d290929dc740
Jerry Jean-Pierre <jeanpierrejerry@gmail.com>
2018-07-26 11:56:26 -0400

    Aligned text and image

--- main.py
+++ main.py
@@ -27,13 +27,23 @@ class MainHandler(webapp2.RequestHandler):
 
 #Writes out the HTML to create the post boxes
         for post in sorted_posts:
-            self.response.write("<div class= 'box'>")
-            self.response.write("<div class= 'post_info'>")
-            self.response.write("<h2>" + post.title + "</h2>")
-            self.response.write("<p></p><h3>" + post.desc + "</h3>")
-            self.response.write("<p></p><p></p><h3>" + post.location + "</h3> </div>")
             if post.image:
-                self.response.write("<p></p> <img class='picture' src='/img?id=" + str(post.key.id()) + "'>")
+                self.response.write("<div class= 'box'><table><tbody><tr><td>")
+            else:
+                self.response.write("<div class= 'box'>")
+            if post.image:
+                self.response.write("<img class='picture' src='/img?id=" + str(post.key.id()) + "'>")
+                self.response.write("</td><td style='width: 50%;'>")
+                self.response.write("<div class= 'image_info'>")
+                self.response.write("<h2>" + post.title + "</h2>")
+                self.response.write("<p></p><h3>" + post.desc + "</h3>")
+                self.response.write("<p></p><p></p><h3>" + post.location + "</h3></div>")
+                self.response.write("</td></tr></tbody></table>")
+            else:
+                self.response.write("<div class= 'post_info'>")
+                self.response.write("<h2>" + post.title + "</h2>")
+                self.response.write("<p></p><h3>" + post.desc + "</h3>")
+                self.response.write("<p></p><p></p><h3>" + post.location + "</h3></div>")
             self.response.write("</div>")
             self.response.write("<br></br>")
 
